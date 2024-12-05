883b4fd25e1a263dff1f1efb3ed4176e0770bd66
mapachxn <agustmx1306@gmail.com>
2024-12-04 18:30:06 -0600

    permite accesos

--- drf/settings.py
+++ drf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--g5rju-cw_@2w0f)%zzzful!q4_$w!hmet#6o+e^=(=77a54y=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herboadan-b1800f8e16ba.herokuapp.com']
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
