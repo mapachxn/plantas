8ce0f271648f472cc047c4fd6ce2d4ae59d26b8d
mapachxn <agustmx1306@gmail.com>
2024-12-04 19:45:45 -0600

    endpoint

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Planta(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
-    imagen = models.ImageField(upload_to='imagenes/', null=True, blank=True)  # Nuevo campo para la imagen
+    imagen = models.ImageField(upload_to='imagenes/', null=True, blank=True)  # Campo para la imagen
 
     def __str__(self):
         return self.nombre
