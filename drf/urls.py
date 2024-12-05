a2de73f26eeec5396554f5b4004a572bf77d6763
mapachxn <agustmx1306@gmail.com>
2024-12-05 02:00:18 -0600

    add Remedios2

--- drf/urls.py
+++ drf/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls'))
+    path('api/v1/', include('api.urls')),
+    path('api/', include('remedios.urls'))
 ]
 
 if settings.DEBUG:
